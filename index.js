049daf185655e007624af27b971ec84886f07f00
Valentin <contact@eywek.fr>
2016-12-28 22:34:58 +0100

    add peak times

--- index.js
+++ index.js
@@ -62,11 +62,82 @@ app.get('/max', function (req, res) {
       if (err) console.error(err)
       return res.json({max: 0})
     }
-    res.json({max: rows.count})
+    res.json({max: rows[0].count})
   })
 })
-// Peak hours
-// Peak days
+// Peak times - hours
+app.get('/stats/peak-times/hours', function (req, res) {
+  console.info('[' + new Date() + '] Request peak times hours from ' + req.ip)
+  res.setHeader('Content-Type', 'application/json') // is json
+  // query
+  db.query('SELECT HOUR(time) AS `hour`, AVG(count) AS `average_players`' +
+    'FROM players' +
+    'GROUP BY HOUR(time)' +
+    'ORDER BY AVG(count) DESC' +
+    'LIMIT 5',
+  function (err, rows, fields) {
+    if (err || rows === undefined || rows.length === 0) {
+      if (err) console.error(err)
+      return res.json([])
+    }
+    // formatting
+    var data = {}
+    for (var i = 0; i < rows.length; i++) {
+      data[rows[i].hour] = Math.round(rows[i].average_players)
+    }
+    res.json(data)
+  })
+})
+// Peak times - days
+app.get('/stats/peak-times/days', function (req, res) {
+  console.info('[' + new Date() + '] Request peak times days from ' + req.ip)
+  res.setHeader('Content-Type', 'application/json') // is json
+  // query
+  db.query('SELECT WEEKDAY(time) AS `day`, AVG(count) AS `average_players`, time' +
+    'FROM players' +
+    'GROUP BY WEEKDAY(time)' +
+    'ORDER BY AVG(count) DESC' +
+    'LIMIT 5',
+  function (err, rows, fields) {
+    if (err || rows === undefined || rows.length === 0) {
+      if (err) console.error(err)
+      return res.json([])
+    }
+    // formatting
+    var data = {}
+    var day
+    for (var i = 0; i < rows.length; i++) {
+      day = (new Date(rows[i].time)).getDay()
+      switch (day) {
+        case 0:
+          day = 'Dimanche'
+          break
+        case 1:
+          day = 'Lundi'
+          break
+        case 2:
+          day = 'Mardi'
+          break
+        case 3:
+          day = 'Mercredi'
+          break
+        case 4:
+          day = 'Jeudi'
+          break
+        case 5:
+          day = 'Vendredi'
+          break
+        case 6:
+          day = 'Samedi'
+          break
+        default:
+          break
+      }
+      data[day] = Math.round(rows[i].average_players)
+    }
+    res.json(data)
+  })
+})
 
 // ==========
 // LISTEN
